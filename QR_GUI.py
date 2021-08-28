b1e1d7dec99b9da66e9e3d04b000c958c4e57a1b
Gera097 <84435213+Gera097@users.noreply.github.com>
2021-08-28 18:03:34 -0500

    Agregada lectura de archivos de Excel

--- QR_GUI.py
+++ QR_GUI.py
@@ -2,16 +2,25 @@
 #Gerardo Sánchez Alba
 
 import PySimpleGUI as sg
+import pandas as pd
+import os.path
 
-#Recipe - Get Filename With No Input Display. Returns when file selected-----------------
+#Recipe - Get Filename With No Input Display. Returns when file selected
 sg.theme()
 
-layout = [[sg.Text('Elige un archivo con extensión CSV')],
-          [sg.Input(key='-FILE-', visible=False, enable_events=True), sg.FileBrowse(button_text='Buscar',file_types=(("CSV", "*.csv"), ))]]
+layout = [[sg.Text('Elige un archivo con extensión CSV o un Libro de Excel')],
+          [sg.Input(key='-FILE-', visible=False, enable_events=True), sg.FileBrowse(button_text='Buscar',file_types=(("Archivo CSV", "*.csv"),("Libro de Excel", "*.xlsx"),),pad = (150,10))]]
 
-event, values = sg.Window('Buscar archivo', layout).read(close=True)
+event, values = sg.Window('Generador_QR', layout).read(close=True)
 
 print(f'You chose: {values["-FILE-"]}')
 
+#Leer datos del archivo desde la ruta seleccionada por el usuario
+nombre_archivo, extension = os.path.splitext(str(values["-FILE-"]))
 
-#----------------------------------------------------------------------------------------
+if extension == ".csv":
+    datos = pd.read_csv(values["-FILE-"],header = 0)
+
+elif extension == ".xlsx":
+    datos = pd.read_excel(values["-FILE-"])
+   
