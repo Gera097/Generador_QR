fa63d8e3f1ee3663a1c7a3664978e0ac9b52956b
Gera097 <84435213+Gera097@users.noreply.github.com>
2021-08-29 00:53:30 -0500

    Versión parcial finalizada, creación de los QR en una carpeta con el nombre de la columna seleccionada

--- QR_GUI.py
+++ QR_GUI.py
@@ -3,7 +3,7 @@
 
 import PySimpleGUI as sg
 import pandas as pd
-import os.path
+import os
 import qrcode
 
 #Tipo de archivos que admitimos para lectura
@@ -18,7 +18,7 @@ layout = [[sg.Text('Elige un archivo con extensión CSV o un Libro de Excel')],
 event, values = sg.Window('Generador_QR', layout).read(close=True)
 
 
-print(f'You chose: {values["-FILE-"]}')
+#print(f'You chose: {values["-FILE-"]}')
 
 #Leer datos del archivo desde la ruta seleccionada por el usuario
 nombre_archivo, extension = os.path.splitext(str(values["-FILE-"]))
@@ -34,7 +34,7 @@ else:
 #Elegir columna que se desea convertir a QR
 diccionario_de_datos = datos.to_dict()
 nombre_columnas = [nombre for nombre in diccionario_de_datos.keys()]
-print(nombre_columnas)
+#print(nombre_columnas)
 
 layout = [  [sg.Text('¿De qué columna desea obtener los códigos QR?')],
             [sg.Listbox(nombre_columnas, size=(15, len(nombre_columnas)), key='-NOMBRE_COLUMNA-')],
@@ -50,8 +50,23 @@ while True:                  # the event loop
         break
     if event == 'Ok':
         if values['-NOMBRE_COLUMNA-']:  
-            window['-COLUMNA_ELEGIDA-'].update(values['-NOMBRE_COLUMNA-'])  # if something is highlighted in the list
-
+            window['-COLUMNA_ELEGIDA-'].update(values['-NOMBRE_COLUMNA-'][0])  # if something is highlighted in the list
+            columna_elegida = values['-NOMBRE_COLUMNA-'][0]
+            break
 window.close()
+#Creación de carpeta para la columna seleccionada
+nombre_columna = str(columna_elegida).title()
+nombre_carpeta = nombre_columna.upper().replace(" ","_")+"_QR"
+if nombre_carpeta not in os.listdir("./"):
+    os.makedirs(nombre_carpeta)
 
 #Creación de códigos QR para la columna seleccionada en el directorio elegido
+def generar_qr(diccionario_de_datos, columna_elegida, nombre_carpeta = ""):
+    for index,dato in enumerate(diccionario_de_datos[columna_elegida].values()):
+        imagen = qrcode.make(str(dato))
+        f = open(nombre_carpeta+"./"+nombre_columna+"_"+str(index)+".png","wb")
+        imagen.save(f)
+        f.close()
+
+generar_qr(diccionario_de_datos, columna_elegida, nombre_carpeta)
+
