452a5044923bbfb9e936b17708434618a4cfe406
Gera097 <84435213+Gera097@users.noreply.github.com>
2021-08-30 23:28:01 -0500

    Pequeño rediseño de las ventanas del programa

--- QR_GUI.py
+++ QR_GUI.py
@@ -3,8 +3,7 @@
 
 import PySimpleGUI as sg
 import pandas as pd
-import os
-import qrcode
+import os,qrcode,sys
 
 #Tipo de archivos que admitimos para lectura
 FILETYPES = (("Libro de Excel", "*.xlsx"),("Archivo CSV", "*.csv")) 
@@ -13,15 +12,31 @@ FILETYPES = (("Libro de Excel", "*.xlsx"),("Archivo CSV", "*.csv"))
 sg.theme()
 
 layout = [[sg.Text('Elige un archivo con extensión CSV o un Libro de Excel')],
-          [sg.Input(key='-FILE-', visible=False, enable_events=True), sg.FileBrowse(button_text='Buscar',file_types=(FILETYPES),pad = (150,10))]]
+          [sg.Input(key='-FILE-', visible=False, enable_events=True), sg.FileBrowse(button_text='Buscar',file_types=(FILETYPES),pad = (150,10))],
+          [sg.Text("Elegiste: "),sg.Text(size=(30,1), key='-ARCHIVO_ELEGIDO-')],
+          [sg.Button('Continuar')]]
 
-event, values = sg.Window('Generador_QR', layout).read(close=True)
+window_buscar_archivo = sg.Window('Generador_QR', layout,element_justification="center")
 
 
+while True:                  # the event loop
+    event, values = window_buscar_archivo.read()
+    if event == sg.WIN_CLOSED:
+        sys.exit("Se cerró el programa")
+    if event == '-FILE-':
+        nombre_archivo = values['-FILE-'].split("/")
+        window_buscar_archivo['-ARCHIVO_ELEGIDO-'].update(nombre_archivo[-1])
+    if event == 'Continuar':
+        if values['-FILE-']:  
+              # if something is highlighted in the list
+            archivo_elegido = values['-FILE-']
+            break
+    
+window_buscar_archivo.close()
 #print(f'You chose: {values["-FILE-"]}')
 
 #Leer datos del archivo desde la ruta seleccionada por el usuario
-nombre_archivo, extension = os.path.splitext(str(values["-FILE-"]))
+nombre_archivo, extension = os.path.splitext(str(archivo_elegido))
 
 
 if extension == ".csv":
@@ -42,23 +57,23 @@ nombre_columnas = [nombre for nombre in diccionario_de_datos.keys()]
 #print(nombre_columnas)
 
 layout = [  [sg.Text('¿De qué columna desea obtener los códigos QR?')],
-            [sg.Listbox(nombre_columnas, size=(15, len(nombre_columnas)), key='-NOMBRE_COLUMNA-')],
-            [sg.Button('Ok')],
-            [sg.Text("Elegiste: "),sg.Text(size=(15,1), key='-COLUMNA_ELEGIDA-')]]
+            [sg.Listbox(nombre_columnas, size=(45, len(nombre_columnas)), key='-NOMBRE_COLUMNA-')],
+            [sg.Button('Ok')]]
+            #[sg.Text("Elegiste: "),sg.Text(size=(15,1), key='-COLUMNA_ELEGIDA-')]]
             
 
-window = sg.Window('Elige un nombre de columna', layout)
+window_elegir_columna = sg.Window('Elige un nombre de columna', layout, element_justification="center")
 
 while True:                  # the event loop
-    event, values = window.read()
+    event, values = window_elegir_columna.read()
     if event == sg.WIN_CLOSED:
-        break
+        sys.exit("Se cerró el programa")
     if event == 'Ok':
         if values['-NOMBRE_COLUMNA-']:  
-            window['-COLUMNA_ELEGIDA-'].update(values['-NOMBRE_COLUMNA-'][0])  # if something is highlighted in the list
+            #window_elegir_columna['-COLUMNA_ELEGIDA-'].update(values['-NOMBRE_COLUMNA-'][0])  # if something is highlighted in the list
             columna_elegida = values['-NOMBRE_COLUMNA-'][0]
             break
-window.close()
+window_elegir_columna.close()
 #Creación de carpeta para la columna seleccionada
 nombre_columna = str(columna_elegida).title()
 nombre_carpeta = nombre_columna.upper().replace(" ","_")+"_QR"
@@ -81,7 +96,7 @@ def generar_qr(diccionario_de_datos, columna_elegida, nombre_carpeta = ""):
 n_datos = generar_qr(diccionario_de_datos, columna_elegida, nombre_carpeta)
 
 layout = [[sg.Text("Los códigos QR fueron generados en la carpeta: " + nombre_carpeta)],
-            [sg.Text("Se crearon: " + str(n_datos) + "QRs")],
-            [sg.Text("En la dirección: " + str(os.getcwd()))]]
+            [sg.Text("Se crearon: " + str(n_datos) + " QRs")],
+            [sg.Text("En la dirección: " + str(os.getcwd()) + "\\resultados\\" + nombre_carpeta)]]
 
 event, values = sg.Window('Generador_QR', layout).read(close=True)
